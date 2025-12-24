d3d3c5417f99fdb19e2d07d82504309b7e4f328d
Utkarsh-Nexus <utkarsh270599@gmail.com>
2025-12-25 04:43:59 +0530

    feat(terraform): update azurerm_storage_account module

--- modules/azurerm_storage_account/main.tf
+++ modules/azurerm_storage_account/main.tf
@@ -1,11 +1,12 @@
-resource "azurerm_storage_account" "stg" {
-  for_each = var.storage_accounts
-
-  name                     = each.value.name
-  resource_group_name      = each.value.resource_group_name
-  location                 = each.value.location
-  account_tier             = each.value.account_tier
-  account_replication_type = each.value.account_replication_type
-
-  tags = each.value.tags
+variable "storage_accounts" {
+  type = map(object({
+    name                     = string
+    rg_key                   = string
+    location                 = string
+    account_tier             = string
+    account_replication_type = string
+    tags                     = map(string)
+  }))
 }
+
+variable "rg_names" { type = map(string) }
--- modules/azurerm_storage_account/variables.tf
+++ modules/azurerm_storage_account/variables.tf
@@ -1,10 +1,10 @@
-variable "storage_accounts" {
-  type = map(object({
-    name                     = string
-    resource_group_name      = string
-    location                 = string
-    account_tier             = string
-    account_replication_type = string
-    tags                     = optional(map(string))
-  }))
+resource "azurerm_storage_account" "sa" {
+  for_each = var.storage_accounts
+
+  name                     = each.value.name
+  resource_group_name      = var.rg_names[each.value.rg_key]
+  location                 = each.value.location
+  account_tier             = each.value.account_tier
+  account_replication_type = each.value.account_replication_type
+  tags                     = each.value.tags
 }
